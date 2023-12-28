56c050ffa41dae2e08d44d287a75c3c89ddf715a
Carseason <757841203@qq.com>
2023-12-28 19:22:21 +0800

    new luci-app-mfun

---
+++ luci-app-mfun/luasrc/model/cbi/mfun.lua
@@ -0,0 +1,34 @@
+--[[
+LuCI - Lua Configuration Interface
+]]--
+
+local taskd = require "luci.model.tasks"
+local m, s, o
+
+m = taskd.docker_map("mfun", "mfun", "/usr/libexec/istorec/mfun.sh",
+	translate("Mfun"),
+	translate("Mfun is an ultimedia program."))
+
+s = m:section(SimpleSection, translate("Service Status"), translate("Mfun status:"))
+s:append(Template("mfun/status"))
+
+s = m:section(TypedSection, "mfun", translate("Setup"), translate("The following parameters will only take effect during installation or upgrade:"))
+s.addremove=false
+s.anonymous=true
+
+o = s:option(Value, "port", translate("HTTP Port").."<b>*</b>")
+o.rmempty = false
+o.default = "8990"
+o.datatype = "port"
+
+o = s:option(Value, "config_path", translate("Config path").."<b>*</b>")
+o.rmempty = false
+o.default = ""
+o.datatype = "string"
+
+o = s:option(Value, "tmp_path", translate("Tmp path").."<b>*</b>")
+o.rmempty = false
+o.default = ""
+o.datatype = "string"
+
+return m
---
+++ luci-app-mfun/root/usr/libexec/istorec/mfun.sh
@@ -0,0 +1,82 @@
+#!/bin/sh
+
+ACTION=${1}
+shift 1
+
+get_image() {
+  IMAGE_NAME="carseason/mfun:latest"
+}
+
+do_install() {
+  get_image
+  echo "docker pull ${IMAGE_NAME}"
+  docker pull ${IMAGE_NAME}
+  docker rm -f mfun
+
+  do_install_detail
+}
+
+do_install_detail() {
+  local config_path=`uci get mfun.@mfun[0].config_path 2>/dev/null`
+  local tmp_path=`uci get mfun.@mfun[0].tmp_path 2>/dev/null`
+  local port=`uci get mfun.@mfun[0].port 2>/dev/null`
+
+  if [ -z "$config_path" ]; then
+      echo "config path is empty!"
+      exit 1
+  fi
+  if [ -z "$tmp_path" ]; then
+      echo "tmp path is empty!"
+      exit 1
+  fi
+
+  [ -z "$port" ] && port=8990
+
+  local cmd="docker run --restart=unless-stopped -d \
+    -v \"$config_path:/mfun/store\" \
+    -v \"$tmp_path:/mfun/tmp\" \
+    -v \"/mnt:/mnt\" \
+    -p $port:8990"
+
+  mountpoint -q /mnt && cmd="$cmd:rslave"
+  cmd="$cmd --name mfun \"$IMAGE_NAME\""
+
+  echo "$cmd"
+  eval "$cmd"
+
+}
+
+usage() {
+  echo "usage: $0 sub-command"
+  echo "where sub-command is one of:"
+  echo "      install                Install the mfun"
+  echo "      upgrade                Upgrade the mfun"
+  echo "      rm/start/stop/restart  Remove/Start/Stop/Restart the mfun"
+  echo "      status                 Mfun status"
+  echo "      port                   Mfun port"
+}
+
+case ${ACTION} in
+  "install")
+    do_install
+  ;;
+  "upgrade")
+    do_install
+  ;;
+  "rm")
+    docker rm -f mfun
+  ;;
+  "start" | "stop" | "restart")
+    docker ${ACTION} mfun
+  ;;
+  "status")
+    docker ps --all -f 'name=mfun' --format '{{.State}}'
+  ;;
+  "port")
+    docker ps --all -f 'name=mfun' --format '{{.Ports}}' | grep -om1 '0.0.0.0:[0-9]*' | sed 's/0.0.0.0://'
+  ;;
+  *)
+    usage
+    exit 1
+  ;;
+esac
