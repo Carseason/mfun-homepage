1d7bb07ad44d60ad450de6509fed3f178b143b68
Carseason <757841203@qq.com>
2023-12-29 14:48:52 +0800

    Default User

--- luci-app-mfun/luasrc/model/cbi/mfun.lua
+++ luci-app-mfun/luasrc/model/cbi/mfun.lua
@@ -7,7 +7,9 @@ local m, s, o
 
 m = taskd.docker_map("mfun", "mfun", "/usr/libexec/istorec/mfun.sh",
 	translate("Mfun"),
-	translate("Mfun is an ultimedia program."))
+	translate("Mfun is an ultimedia program.")
+	.. translate("Default User") 
+	.. ': admin password')
 
 s = m:section(SimpleSection, translate("Service Status"), translate("Mfun status:"))
 s:append(Template("mfun/status"))
